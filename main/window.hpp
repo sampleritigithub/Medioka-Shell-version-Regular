e179d2503de99195fd5cbda1d2a38d1a625a8345
Omar Ismail_Source <103457750+sampleritigithub@users.noreply.github.com>
2022-04-13 13:28:00 +0200

    Window Libraries Version 0.1.0
    Window Libraries

---
+++ main/window.hpp
@@ -0,0 +1,35 @@
+/****Copyrights:****\
+ *Omar Ismail 2022 *
+\*******************/
+int bar() {
+  while (true) {
+    /********************\
+     *Window bar        *
+     *Window Options (X)*
+    \********************/
+    window = "Regular Window";
+    state = window;
+    if (state == window) {
+      
+    }
+    else{
+      break; 
+    }
+  }
+}
+int rmen() {
+  while (true) {
+     /***************\
+     *Window bar (X)*
+     *Window Options*
+    \****************/
+    window = "Regular Window";
+    state = window;
+    if (state == window) {
+      
+    }
+    else{
+      break; 
+    }
+  }
+}
