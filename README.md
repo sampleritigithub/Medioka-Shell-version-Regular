a4c6dc173b3db2e69d7bf915cb3fcc8ecd3e21cd
Tobey_Source <103457750+sampleritigithub@users.noreply.github.com>
2022-06-20 02:01:44 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Medioka-Shell-version-Regular
-Regular shell for the Medioka OS (MedOS Shell(Regular)) 
+System Shell
+-
+
+The main interface
---
+++ explorer.cpp
@@ -0,0 +1,5 @@
+#include "..\medoskrnl\print.h"
+PrintString("Welcome!");
+PrintString("TrustedInstaller\n");
+PrintString("-/-/-/-/-/-/-/-/-/-/-/-/-/");
+
